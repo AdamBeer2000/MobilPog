4eaa88758d96c808818d883d34f670e7489d29a2
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-10-04 13:57:46 +0200

    Update Barricade.kt

--- app/src/main/java/com/mobilpogbead/entity/Barricade.kt
+++ app/src/main/java/com/mobilpogbead/entity/Barricade.kt
@@ -1,6 +1,23 @@
 package com.mobilpogbead.entity
 
-class Barricade//(coordinates: Pair<Int, Int>):Entity( coordinates)
+class Barricade(private var x: Int, private var y: Int):Entity(x, y)
 {
+    override var speed: Int
+        get() = this.speed
+        set(value) { this.speed = value }
+
+    override var hp: Int
+        get() = this.hp
+        set(value) { this.hp = value }
+
+    override var hitbox: Array<Array<Byte>>
+        get() = this.hitbox
+        set(value) { this.hitbox = value }
+
+
+    override fun hit() {
+        this.hp = 0
+    }
+
 
 }
