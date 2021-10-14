75c8c03106edbe42e62226152c55e896646d8052
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-10-14 16:53:46 +0200

    audio

---
+++ app/src/main/java/com/mobilpogbead/audio/AudioManager.kt
@@ -0,0 +1,29 @@
+package com.mobilpogbead.audio
+
+import android.content.Context
+import android.media.MediaPlayer
+import com.mobilpogbead.R
+
+class AudioManager(private val context: Context)
+{
+    var mediaPlayer: MediaPlayer? = null
+
+    fun playBeliever()
+    {
+        if(mediaPlayer == null)
+        {
+            mediaPlayer = MediaPlayer.create(context, R.raw.believer)
+            mediaPlayer!!.isLooping = false;
+            mediaPlayer!!.start()
+        }
+        else
+        {
+            mediaPlayer!!.start()
+        }
+    }
+
+    fun playBoom(){}
+    fun playShoot(){}
+    fun playWin(){}
+    fun playGameOver(){}
+}
