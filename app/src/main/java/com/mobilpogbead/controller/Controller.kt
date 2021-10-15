b9d3899f84fe33196708e972401563d995fa9afe
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-10-15 12:10:16 +0200

    typo fix

--- app/src/main/java/com/mobilpogbead/controller/Controller.kt
+++ app/src/main/java/com/mobilpogbead/controller/Controller.kt
@@ -26,10 +26,6 @@ class Controller(private var context: Context,var boundaries:Boundaries,
     val model=Model(boundaries, context)
     val view=View(model,pointcounter,lifecounter,deathanim, context)
 
-    //var gyro_x: Double = 0.0
-    //var gyro_y: Double = 0.0
-    //var gyro_z: Double = 0.0
-
     private var mValuesMagnet = FloatArray(3)
     private var mValuesAccel = FloatArray(3)
     private var mValuesOrientation = FloatArray(3)
@@ -39,15 +35,11 @@ class Controller(private var context: Context,var boundaries:Boundaries,
     fun setUpSensor()
     {
         sensorManager = this.context.getSystemService(SENSOR_SERVICE) as SensorManager
-        val gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)//TYPE_GYROSCOPE_UNCALIBRATED
+        val gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
 
         val magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
         val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
-        //sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)//TYPE_GYROSCOPE_UNCALIBRATED
-
-        //sensorManager.registerListener(gyroSensorListener,
-          //  gyroSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
         sensorManager.registerListener(gyroSensorListener,
             magneticSensor, SensorManager.SENSOR_DELAY_FASTEST);
@@ -67,20 +59,6 @@ class Controller(private var context: Context,var boundaries:Boundaries,
 
     var gyroSensorListener: SensorEventListener = object : SensorEventListener {
         override fun onSensorChanged(p0: SensorEvent?) {
-            /*if(p0?.sensor?.type == Sensor.TYPE_GYROSCOPE)//TYPE_GYROSCOPE_UNCALIBRATED
-            {
-                val rightLeft = p0.values[0]
-                val upDown = p0.values[1]
-                val frontBehind = p0.values[2]
-
-                Log.d("SENSOR ","x: $rightLeft")
-                Log.d("SENSOR ","y: $upDown")
-                Log.d("SENSOR ","z: $frontBehind")
-
-                gyro_x = p0.values[0].toDouble()
-                gyro_y = p0.values[1].toDouble()
-                gyro_z = p0.values[2].toDouble()
-            }*/
 
             when (p0?.sensor?.type) {
                 Sensor.TYPE_ACCELEROMETER -> {
